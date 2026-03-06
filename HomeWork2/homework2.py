042ad04c782f1e65ac70952b09ad4d384269c448
Pavel Bordukov <pawwel611@mail.ru>
2026-03-06 14:00:12 +0300

    Tree

--- HomeWork2/homework2.py
+++ HomeWork2/homework2.py
@@ -1,18 +1,22 @@
 # Привести к целому типу -1.6,2.99
 
-print(int(-1.6)) #Вывод: -1
-print(int(2.99)) #Вывод: 2
+print(int(-1.6))  # Вывод: -1
+print(int(2.99))  # Вывод: 2
 
 # Заменить символ "#" на символ "/" в строке 'www.my_site.com#about'
 
-org_url = 'www.my_site.com#about' # Заменяем '#' на '/'
-new_url = org_url.replace('#','/') 
-print(new_url) # Вывод: www.my_site.com/about
+org_url = 'www.my_site.com#about'  # Заменяем '#' на '/'
+new_url = org_url.replace('#', '/')
+print(new_url)  # Вывод: www.my_site.com/about
 
-#напишите программу,которая добавляет 'ing' к слову 'stroka'
+# напишите программу,которая добавляет 'ing' к слову 'stroka'
 
 str_1 = 'Stroka'
 print(str_1 + 'ing')
 
+# В строке "Ivanou Ivan" поменяйте местами слова=> "Ivan Ivanou"
+# Напишите программу которая удаляет в пробел в начале,в конце строки
 
-
+name = 'Ivanou Ivan'
+a, b = name.split()
+print(b, a)
