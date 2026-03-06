46079be4a2ea933b63aabf0c9538cc84aec512ab
Pavel Bordukov <pawwel611@mail.ru>
2026-03-06 13:29:46 +0300

    Initial

---
+++ HomeWork2/homework2.py
@@ -0,0 +1,14 @@
+# Привести к целому типу -1.6,2.99
+
+print(int(-1.6)) #Вывод: -1
+print(int(2.99)) #Вывод: 2
+
+# Заменить символ "#" на символ "/" в строке 'www.my_site.com#about'
+
+org_url = 'www.my_site.com#about' # Заменяем '#' на '/'
+new_url = org_url.replace('#','/') 
+print(new_url) # Вывод: www.my_site.com/about
+
+
+
+
